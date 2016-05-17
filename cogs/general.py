cc640b951a13f3763a1c1d3c3644d0083a76bbb1
ExtraRandom <crazyblob11@gmail.com>
2016-05-18 00:48:52 +0100

    !rd improvement
    Allows for easily changing which order
    Also added two new games

--- cogs/general.py
+++ cogs/general.py
@@ -89,20 +89,30 @@ class General:
         bf_rd = datetime(2016, 10, 21, 0, 0, 0)
         nms_rd = datetime(2016, 6, 21, 0, 0, 0)
         d2_rd = datetime(2016, 11, 11, 0, 0, 0)
+        dx_rd = datetime(2016, 8, 23, 0, 0, 0)
+        hi_rd = datetime(2016, 6, 6, 0, 0, 0)
 
-        ow_days, ow_hrs, ow_mins = rd_calc(ow_rd)
+        ow_days, ow_hrs, ow_mins = rd_calc(ow_rd) # OW and TW:W have the same release date
         bf_days, bf_hrs, bf_mins = rd_calc(bf_rd)  # BF1 and Civ6 have the same release date
         nms_days, nms_hrs, nms_mins = rd_calc(nms_rd)
         d2_days, d2_hrs, d2_mins = rd_calc(d2_rd)
+        dx_days, dx_hrs, dx_mins = rd_calc(dx_rd)
+        hi_days, hi_hrs, hi_mins = rd_calc(hi_rd)
 
-        full_msg = "__**Release Dates List** - *Times, Dates and Games are subject to change*__ \n \n" \
-                   "***Overwatch*** releases in {},{} hours and {} minutes.\n" \
-                   "***Battlefield*** ***1*** and ***Civilization*** ***6*** release in {},{} hours and {} minu" \
-                   "tes. \n" \
-                   "***No Man's Sky*** releases in {},{} hours and {} minutes.\n" \
-                   "***Dishonored*** ***2*** releases in {},{} hours and {} minutes.\n" \
-                   "".format(ow_days, ow_hrs, ow_mins, bf_days, bf_hrs, bf_mins, nms_days, nms_hrs, nms_mins,
-                             d2_days, d2_hrs, d2_mins)
+        # template: "*** *** releases in {},{} hours and {} minutes.\n".format(_days, _hrs, _mins)
+
+        title_msg = "__**Release Dates List** - *Times, Dates and Games are subject to change*__ \n \n"
+        ow_msg = "***Overwatch*** and ***Total War: Warhammer*** release in {},{} hours and {} minutes.\n".format(ow_days, ow_hrs, ow_mins)
+        nms_msg = "***No Man's Sky*** releases in {},{} hours and {} minutes.\n".format(nms_days, nms_hrs, nms_mins)
+        bf_msg = "***Battlefield*** ***1*** and ***Civilization*** ***6*** release in {},{} hours and {} minu" \
+                 "tes. \n".format(bf_days, bf_hrs, bf_mins)
+        d2_msg = "***Dishonored*** ***2*** releases in {},{} hours and {} minutes.\n".format(d2_days, d2_hrs, d2_mins)
+        dx_msg = "***Deus Ex: Mankind Divided*** releases in {},{} hours and {} minutes.\n".format(dx_days, dx_hrs,
+                                                                                                   dx_mins)
+        hi_msg = "***Hearts of Iron*** ***4*** releases in {},{} hours and {}minutes.\n".format(hi_days, hi_hrs, hi_mins)
+        # having it like this should make changing them easier compared to how it was previously set up
+
+        full_msg = title_msg + ow_msg + hi_msg + nms_msg + dx_msg + bf_msg + d2_msg
 
         await s.destructmsg(full_msg, 60, self.bot)
 
