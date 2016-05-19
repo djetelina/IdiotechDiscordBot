5af04c521210dac20a20cbccffe8b4948017b121
ExtraRandom <crazyblob11@gmail.com>
2016-05-19 21:49:54 +0100

    Template for token.py - Has comments on where to find the API Keys / Access Tokens

---
+++ token_template.py
@@ -0,0 +1,29 @@
+"""
+Strings for various tokens and API keys required by some commands
+
+Don't forget to rename this file or create a copy named "tokens.py"
+"""
+
+token = ""
+"""
+Found here: https://discordapp.com/developers/applications/me
+The token for the Discord Bot, Will require creating an Application.
+The token is found under "APP BOT USER" and then "Token:"
+"""
+
+fb_key = ""
+"""
+First go here: https://developers.facebook.com/
+Make an new App (basic set-up is fine, use communication as category)
+Then go here: https://developers.facebook.com/tools/explorer/
+To the top right you should see "Graph API Explorer", click that and select the app you just made.
+After that click the "Get Token" button and then "Get App Token"
+The string in the text box labeled "Access token" is your key
+"""
+
+yt_key = ""
+"""
+Found here: https://console.developers.google.com/apis/library
+Create a new project, then on the API overviews go to YouTube Data API and enable it.
+Then go to Credentials and create an API Key there.
+"""
