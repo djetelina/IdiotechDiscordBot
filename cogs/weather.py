d8a938a2b18641ef93fd2d167c1519bb22e9204d
ExtraRandom <crazyblob11@gmail.com>
2016-05-21 06:11:25 +0100

    Small improvements to weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -6,14 +6,11 @@ REQUIRES INSTALLATION OF PYCOUNTRY
 ================================================
 """
 
-
 from discord.ext import commands
 import simplify as s
 import aiohttp
 import tokens as t
 import pycountry
-
-# Remove ones not being used once done
 
 
 class Weather:
@@ -30,6 +27,7 @@ class Weather:
         # print(spaces)  # debug - remove
         if not spaces:
             await s.destructmsg("***Getting Weather Status for {}. This make take a while.***".format(loc), 15, self.bot)
+            # seems to take around 15 seconds to find weather information
 
             weather_api = self.base_url + loc + self.a_url + self.key
             # print(weather_api)
@@ -39,19 +37,20 @@ class Weather:
                     data = await resp.json()
                     # print(data)
                     temp = data["main"]["temp"]
-                    brief = data["weather"][0]["main"] # ie cloudy
-                    desc = data["weather"][0]["description"]  # ie very cloud/ overcast clouds and so on
+                    brief = data["weather"][0]["main"]  # ie cloudy
+                    desc = data["weather"][0]["description"]  # ie very cloudy/ overcast clouds and so on
                     name = data["name"]  # will return name without '_' and with capitalised words, saves effort
                     cloud = str(data["clouds"]["all"])
                     country = pycountry.countries.get(alpha2=data["sys"]["country"])
 
-                    cel = kel_to_cel(temp)
-                    fah = cel_to_fah(cel)
+                    desc = capital_everything(desc)
+                    cel = to_1dp(str(kel_to_cel(temp)))
+                    fah = to_1dp(str(cel_to_fah(cel)))
 
                     await s.destructmsg("__**{}, {} - Weather Status**__\n"
                                         "**Temperature:** {}c  -  {}f\n"
                                         "**Weather:** {} - {}\n"
-                                        "**Cloudiness:** {} percent".format(name, country.name, str(cel)[0:5], fah,
+                                        "**Cloudiness:** {} percent".format(name, country.name, cel, fah,
                                                                             brief, desc, cloud), 50, self.bot)
 
         else:
@@ -64,12 +63,52 @@ def setup(bot):
 
 
 def kel_to_cel(kelvin):
-    result = int(kelvin) - 273.15
+    """
+    Converts Kelvin to Celsius
+
+    :param kelvin:
+    :return: Celsius:
+    """
+    result = float(kelvin) - 273.15
     return result
 
 
-def cel_to_fah(cel):
-    result = (int(cel)*1.8) + 32
+def cel_to_fah(cels):
+    """
+    Converts Celsius to Fahrenheit
+
+    :param cels: Celsius
+    :return: Fahrenheit:
+    """
+    result = (float(cels)*1.8) + 32
     return result
 
 
+def capital_everything(string):
+    """
+    Takes a string, such as "hello world" and capitalizes each world
+    returning "Hello World" in this example
+
+    :param string:
+    :return: The capitalized words in a string:
+    """
+    listy = string.split()  # () is same as " " apparently
+    new_list = []
+    for word in listy:
+        thing = word.capitalize()
+        new_list.append(thing)
+    result = ' '.join(new_list)
+    return result
+
+
+def to_1dp(value):
+    """
+    Shortens a number to 1 decimal point
+
+    :param value:
+    :return: string of number to 1 decimal point
+    """
+    before, after = str(value).split(".")
+    after = after[0:1]
+    result = before + "." + after
+    return result
