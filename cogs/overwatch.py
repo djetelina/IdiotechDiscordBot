e08256fa1d8db32dd9ff3bb2fb1cc75a1708fb24
ExtraRandom <crazyblob11@gmail.com>
2016-06-05 00:00:56 +0100

    Updated Overwatch command again

--- cogs/overwatch.py
+++ cogs/overwatch.py
@@ -9,6 +9,7 @@ from helpers import descriptions as desc
 
 log = logging.getLogger(__name__)
 
+
 class Overwatch:
     def __init__(self, bot):
         """
@@ -18,12 +19,6 @@ class Overwatch:
 
     @commands.command(description=desc.ow, brief=desc.owb)
     async def overwatch(self, region: str, battletag: str):
-        games_played = 0
-        games_won = 0
-        time_played = 0
-        count = 0
-        stat_count = 0
-
         msg = await self.bot.say("Fetching Stats for {}".format(battletag))
 
         user = battletag.replace("#", "-")
@@ -58,33 +53,15 @@ class Overwatch:
         doc = bs4.BeautifulSoup(res.text, "html.parser")
         page = doc.select('div')
 
-        # eof = len(page) - 1   # eof = end of file - left in as you could use it for the for loop "stuff in page" maybe
-
         most_played = page[82].select('div')[2].getText()
         most_games = page[82].select('div')[3].getText()
 
-        for stuff in page:
-            try:
-                if str(page[count].select('div')[0].select('td')[0].getText()) == "Games Won":
-                    try:
-                        stats = page[count].select('div')[0].select('td')
+        stats = doc.find_all('td')
+        # print(stats)
 
-                        for stat in stats:
-                            if stats[stat_count].getText() == "Games Won":
-                                games_won = int(stats[stat_count + 1].getText())
-                            if stats[stat_count].getText() == "Games Played":
-                                games_played = int(stats[stat_count + 1].getText())
-                            elif stats[stat_count].getText() == "Time Played":
-                                time_played = stats[stat_count + 1].getText()
-                            stat_count += 1
-
-                    except Exception as e:
-                        log.exception("Parsing HTML")
-
-            except Exception:
-                pass  # This will print a lot of errors if we log it
-
-            count += 1
+        games_won = find_value(stats, "Games Won")
+        games_played = find_value(stats, "Games Played")
+        time_played = find_value(stats, "Time Played")
 
         games_lost = int(games_played) - int(games_won)
         won_lost = "{}/{}".format(games_won, games_lost)
@@ -96,6 +73,52 @@ class Overwatch:
                                          "Most Played Hero:   *{}, {} played*"
                                          "".format(battletag, reg.upper(), time_played, games_played,
                                                    won_lost, most_played, most_games))
+
+
+def find_value(stats, name):
+    """
+    :param stats: stats list
+    :param name: name of value to find (i.e. Games Won)
+    :return: largest value for name (there are multiple game won's but the biggest will be the overall games won
+    rather than a character specific games won
+    """
+
+    tagged_name = "<td>{}</td>".format(name)
+
+    things = []
+    is_time = False
+
+    hour = []
+    mins = []
+
+    for item in stats:
+        if name == "Time Played":
+            is_time = True
+
+            if str(item) == tagged_name:
+                time = item.next_sibling.getText().split()
+                if time[1] == "minutes" or time[1] == "minute":
+                    mins.append(int(time[0]))
+                elif time[1] == "hours" or time[1] == "hour":  # needed instead of else as it can also be in seconds
+                    hour.append(int(time[0]))
+
+        elif str(item) == tagged_name:
+            things.append(int(item.next_sibling.getText()))
+
+    if is_time:
+        if not hour:
+            # meaning max play time is in minutes
+            return "{} minutes".format(max(mins))
+            # doesnt accommodate for 1 min playtime but seriously why would you even have that short a play time
+
+        elif hour:
+            max_time = max(hour)
+            sfx = "hours"
+            if max_time == 1:
+                sfx = "hour"
+            return "{} {}".format(max_time, sfx)
+
+    return max(things)
 
 
 def setup(bot):
