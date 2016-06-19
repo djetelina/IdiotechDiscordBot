c7220d77ea354460c9597a4fbb0ec91e5cbb0e76
David Jetelina <djt@seznam.cz>
2016-06-19 18:36:35 +0200

    more tests and added requirement for psycopg2 (sqlalchemy)

--- tests/maintest.py
+++ tests/maintest.py
@@ -4,12 +4,53 @@ from helpers import settings
 
 class CogsTest(unittest.TestCase):
 
-    def test_load_cogs(self):
-        for extension in settings.extensions:
-            try:
-                bot.load_extension(extension)
-            except Exception as e:
-                self.fail()
+    def test_general(self):
+        try:
+            bot.load_extension('cogs.general')
+        except Exception:
+            self.fail()
+
+    def test_giveaway(self):
+        try:
+            bot.load_extension('cogs.giveaway')
+        except Exception:
+            self.fail()
+
+    def test_restricted(self):
+        try:
+            bot.load_extension('cogs.restricted')
+        except Exception:
+            self.fail()
+
+    def test_versioning(self):
+        try:
+            bot.load_extension('cogs.versioning')
+        except Exception:
+            self.fail()
+
+    def test_games(self):
+        try:
+            bot.load_extension('cogs.games')
+        except Exception:
+            self.fail()
+
+    def test_spamfilter(self):
+        try:
+            bot.load_extension('cogs.spamfilter')
+        except Exception:
+            self.fail()
+
+    def test_currency(self):
+        try:
+            bot.load_extension('cogs.currency')
+        except Exception:
+            self.fail()
+
+    def test_stats(self):
+        try:
+            bot.load_extension('cogs.stats')
+        except Exception:
+            self.fail()
 
 if __name__ == '__main__':
     unittest.main()
